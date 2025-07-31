90345ffe3eec1bb1c78aa2d286495d26be363ae5
bahatisteven <bahatisteven92@gmail.com>
2025-07-31 12:20:39 +0200

    chore: add comments for database configuration

--- config/db.js
+++ config/db.js
@@ -4,9 +4,13 @@ dotenv.config();
 import pg from "pg";
 const { Pool } = pg;
 
-
+// deterine database name based on the environment
 const dbName = process.env.NODE_ENV === "test" ? process.env.DB_NAME_TEST : process.env.DB_NAME;
 
+/**
+ * if the env is test, we"ll use jest to mock the database connection 
+ * to avoid actual database calls during testing
+ */
 let jest;
 if (process.env.NODE_ENV === "test") {
   import ('jest-mock').then(({ default: jestMock }) => {
@@ -14,7 +18,7 @@ if (process.env.NODE_ENV === "test") {
   });
 }
 
-
+// new pool instance to connect to the database
 const dbPool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
@@ -26,6 +30,9 @@ const dbPool = new Pool({
 
 export { dbPool };
 
+/**
+ * if the env is not test, we"ll connect to the database and log the connection status
+ */
 if (process.env.NODE_ENV !== "test") {
 dbPool.query("SELECT NOW()", (err, res) => {
   if (err) {
