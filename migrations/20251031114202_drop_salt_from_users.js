a869b33f3923e942d2b821de8aaea642fdf3ec85
Bahatisteven <bahatisteven92@gmail.com>
2025-10-31 13:47:17 +0200

    feat: Set up database and correct password hashing

---
+++ migrations/20251031114202_drop_salt_from_users.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.table('users', (table) => {
+    table.dropColumn('salt');
+  });
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema.table('users', (table) => {
+    table.string('salt');
+  });
+}
